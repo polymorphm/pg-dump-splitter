89f63341066f47fdfce190d367e3b90f16ff3c73
Andrei Antonov (stdpr.work) <antonov@stdpr.ru>
2018-10-10 13:45:04 +0300

    build can be now with luac

---
+++ embedder/embedder-luac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# vim: set et ts=2 sw=2:
+
+set -e
+
+if [ "x$#" != x6 ]
+then
+  echo 'Illegal number of parameters' 2>&1
+  exit 2
+fi
+
+embedder_py="$1"
+luac="$2"
+plainname="$3"
+input_path="$4"
+output_c_path="$5"
+output_h_path="$6"
+
+"$luac" -o "$output_c_path.luac" -- "$input_path"
+
+exec -- "$embedder_py" "$plainname" "$output_c_path.luac" "$output_c_path" "$output_h_path"
