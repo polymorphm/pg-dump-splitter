aeb1152be5edf2ef0a8241bea295b5e41ccc60dd
Andrei Antonov (stdpr.work) <antonov@stdpr.ru>
2018-12-03 12:02:27 +0300

    EXAMPLE.excluding-obj-hooks* have been updated to new hook signatures

--- EXAMPLE.excluding-obj-hooks1.lua
+++ EXAMPLE.excluding-obj-hooks1.lua
@@ -4,33 +4,35 @@ local export = {}
 
 function export.register_hooks(hooks_ctx)
   function hooks_ctx:add_to_chunk_handler(obj_type, obj_values, directories,
-      filename, order, dump_data)
-    -- we will decide what miss by pattern-matching
+      filename, order, state_keys, state_mem, dump_data)
+    if directories and filename and order and state_keys and dump_data then
+      -- we will decide what to miss by pattern-matching
+      
+      local obj_schema = obj_values.obj_schema or ''
+      local obj_name = obj_values.obj_name or ''
+      local role = obj_values.role or ''
 
-    local obj_schema = obj_values.obj_schema or ''
-    local obj_name = obj_values.obj_name or ''
-    local role = obj_values.role or ''
+      if obj_type == 'grant_function' and
+          obj_schema:match('._taxi_.') and
+          obj_name:match('get_.') and
+          role:match('._client') then
 
-    if obj_type == 'grant_function' and
-        obj_schema:match('._taxi_.') and
-        obj_name:match('get_.') and
-        role:match('._client') then
+        -- exclude these grants!
+        -- them will not be saved to sorted dump chunks
 
-      -- exclude these grants!
-      -- them will not be saved to sorted dump chunks
+        std.print('EXCLUDED:', obj_type,
+            std.table.concat(directories, '/') .. '/' .. filename)
 
-      std.print('EXCLUDED:', obj_type,
+        return
+      end
+
+      -- others will not be ignored
+
+      std.print('PASSED:', obj_type,
           std.table.concat(directories, '/') .. '/' .. filename)
-
-      return
     end
 
-    -- others will not be ignored
-
-    std.print('PASSED:', obj_type,
-        std.table.concat(directories, '/') .. '/' .. filename)
-
-    return directories, filename, order, dump_data
+    return directories, filename, order, state_keys, dump_data
   end
 end
 
--- EXAMPLE.excluding-obj-hooks2.lua
+++ EXAMPLE.excluding-obj-hooks2.lua
@@ -4,14 +4,15 @@ local export = {}
 
 function export.register_hooks(hooks_ctx)
   function hooks_ctx:add_to_chunk_handler(obj_type, obj_values, directories,
-        filename, order, dump_data)
+        filename, order, state_keys, state_mem, dump_data)
+    if directories and filename and order and state_keys and dump_data then
+      -- excluding objects related with partitions.
+      -- looking into ``filename`` looks like the most appropriate method.
 
-    -- excluding objects related with partitions.
-    -- looking into ``filename`` looks like the most appropriate method.
+      if filename:match('%g*_part%d+') then return end
+    end
 
-    if filename:match('%g*_part%d+') then return end
-
-    return directories, filename, order, dump_data
+    return directories, filename, order, state_keys, dump_data
   end
 end
 
